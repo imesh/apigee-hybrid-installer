4990f71eed6095347707ebc6fc6a07bef62b57df
Imesh Gunaratne <imesh.gunaratne@gmail.com>
2021-03-22 12:57:36 +1100

    Print environment variables in uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -4,6 +4,16 @@ set -e
 echo "Setting environment variables..."
 source environment.sh
 
+echo "PROJECT_ID=${PROJECT_ID}"
+echo "REGION=${REGION}"
+echo "AX_REGION=${AX_REGION}"
+echo "ZONE=${ZONE}"
+echo "GKE_CLUSTER_NAME=${GKE_CLUSTER_NAME}"
+echo "APIGEE_HYBRID_VERSION=${APIGEE_HYBRID_VERSION}"
+echo "ENV_NAME=${ENV_NAME}"
+echo "ENV_GROUP_NAME=${ENV_GROUP_NAME}"
+echo "DNS_NAME=${DNS_NAME}"
+
 echo "Setting gcloud config..."
 gcloud config set project $PROJECT_ID
 gcloud config set compute/region $REGION
